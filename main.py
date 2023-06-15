edfdfc40f1a039c28a2473166d20abc13b1cf1d8
Mehul Khanna <mehulkhanna420@gmail.com>
2023-06-15 13:06:51 +0530

    Use a Higher Level API
    Recoded Hand Landmarker to use a higher level solutions API for concise and easy to read code.

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-import cv2
-import numpy as np
-from PIL import Image
-import mediapipe as mp
-from modules.hand_landmarks import detect_hand_landmarks, draw_landmarks
+import cv2 as opencv
+from typing import NamedTuple, List
+from mediapipe.python.solutions import hands, drawing_utils
+from mediapipe.framework.formats.classification_pb2 import ClassificationList
+from mediapipe.framework.formats.landmark_pb2 import (
+    NormalizedLandmarkList,
+    LandmarkList,
+)
 
 
-image = Image.open(r"images/2023-06-13-211745.jpg")
-image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(image))
+class Results(NamedTuple):
+    multi_hand_landmarks: List[NormalizedLandmarkList]
+    multi_hand_world_landmarks: List[LandmarkList]
+    multi_handedness: List[ClassificationList]
 
-result = detect_hand_landmarks(image)
-image = draw_landmarks(image, result)
 
-cv2.imshow("Hand Gesture Recognition", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+hand_landmarker = hands.Hands(
+    static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5
+)
+
+video = opencv.VideoCapture(0)
+while video.isOpened():
+    success, image = video.read()
+    if not success:
+        break
+
+    rgb_image = opencv.cvtColor(image, opencv.COLOR_BGR2RGB)
+    results: Results = hand_landmarker.process(rgb_image)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            drawing_utils.draw_landmarks(image, hand_landmarks, hands.HAND_CONNECTIONS)
+
+    opencv.imshow("Hand Landmarks", image)
+    if opencv.waitKey(5) & 0xFF == ord("q"):
+        break
+
+video.release()
+opencv.destroyAllWindows()
